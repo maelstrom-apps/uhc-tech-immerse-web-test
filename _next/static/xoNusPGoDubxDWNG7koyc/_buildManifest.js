7416ddf3d7ab9c714afd4d31397433a65e6e82be
jason-wilson_uhg <jason-wilson_uhg@users.noreply.github.com>
2023-01-11 21:25:55 +0000

    deploy: uhc-tech/immerse@5aedf6d04245be6bdf5dfc1b32c9257f48b48013

---
+++ _next/static/xoNusPGoDubxDWNG7koyc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:path*",destination:"/"}],fallback:[]},"/":["static/chunks/384-538881d9a583f583.js","static/chunks/pages/index-80f6a3eb0ee6a7ba.js"],"/_error":["static/chunks/pages/_error-a1d5bc3e43249d4c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
