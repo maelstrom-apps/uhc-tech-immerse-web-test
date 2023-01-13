0c1398120005ddeabeb5f8deef0f967b2bacf506
samantha-seifert_uhg <samantha-seifert_uhg@users.noreply.github.com>
2023-01-13 18:15:00 +0000

    deploy: uhc-tech/immerse@a3702f01fad2d93b8dbda23bca25c13566b49e7d

--- _next/static/tov6XyyOkDmHr5bz5xbzI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:path*",destination:"/"}],fallback:[]},"/":["static/chunks/895-9f44b5ea21f11d5b.js","static/chunks/pages/index-05262bfb54e00900.js"],"/_error":["static/chunks/pages/_error-a1d5bc3e43249d4c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
